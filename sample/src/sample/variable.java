4e4404417ecf6e1be353c6fea4e60d6cebfbf721
dir-FY24uchidasoma <SomaUchida()@Dirbato-1388x>
2024-04-08 16:08:18 +0900

    3回目のコミット

---
+++ sample/src/sample/variable.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class variable {
+
+  public static void main(String[] args) {
+    var number1 =2;
+    System.out.println(number1);
+    
+    
+    String str1;
+    str1 = "abc";
+    System.out.print(str1);
+}
+}
